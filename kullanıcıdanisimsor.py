ab313641ff53e4c8aead7bd51f98523c380e9653
Furkan Karakuş <79601715+karakusfurkan27@users.noreply.github.com>
2024-12-23 10:57:45 +0300

    Add files via upload

---
+++ kullanıcıdanisimsor.py
@@ -0,0 +1,7 @@
+# Kullanıcıdan ismini sorma
+isim = input("Lütfen isminizi girin: ")
+
+# Kullanıcıdan beğendiği rengi sorma
+renk = input("Beğendiğiniz rengi girin: ")
+
+print(f"Merhaba {isim}, {renk} rengini seviyorsunuz!")
